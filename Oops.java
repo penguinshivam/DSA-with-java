3fbd7e56197801b510660fd7c88a4ddb6ec758f3
xoshivam <shivaXagarwal@gmail.com>
2024-03-15 18:03:21 +0000

    inhertance in classes

--- Oops.java
+++ Oops.java
@@ -1,90 +1,103 @@
 
     public class Oops {
         public static void main(String args[]){
-            Pen p1 = new Pen();  //constructor,created a pen class p1
-            p1.color = "yellow";
-            p1.setcolor("blue");
-            System.out.println(p1.getcolor());
-            p1. setTip(5);
-            System.out.println(p1.getTip());
-            p1.setcolor("red");
-            // p1.color = "red";
-            System.out.println(p1.getcolor());
+            // Pen p1 = new Pen();  //constructor,created a pen class p1
+            // p1.color = "yellow";
+            // p1.setcolor("blue");
+            // System.out.println(p1.getcolor());
+            // p1. setTip(5);
+            // System.out.println(p1.getTip());
+            // p1.setcolor("red");
+            // // p1.color = "red";
+            // System.out.println(p1.getcolor());
             
-            BankAccount myAcc = new BankAccount();
-            myAcc.username = " fvbnjm";
-            my.password = " dfghj"    // this throw error ,password is a private variable
-            myAcc.setPassword("asdfsdfc");    //we can set password 
+            // BankAccount myAcc = new BankAccount();
+            // myAcc.username = " fvbnjm";
+            // my.password = " dfghj"    // this throw error ,password is a private variable
+            // myAcc.setPassword("asdfsdfc");    //we can set password 
     
     
-            Student s1 = new Student("palak");
-            System.out.println(s1.name);
-            Pen p1 = new Pen("blue");
-            System.out.println(p1.getcolor());
-            System.out.println(p1.color);
-            p1.setcolor("green");
-            Pen p1 = new Pen("green");
-            p1.color = "green";
-             System.out.println(p1.color);
+            // Student s1 = new Student();
+            // s1.name="shivam";
+            // s1.roll=34;
+            // s1.password="abcd";
+            // s1.marks[0]=100;
+            // s1.marks[1]=80;
+            // s1.marks[2]=20;
+            // Student s2 =new Student(s1);
+            // s2.password="hello";
+            // for (int i = 0; i < 3; i++) {
+            //     System.out.println(s2.marks[i]);
+            // }
+            // Fish shark=new Fish();
+            // shark.eat();
+            // Dogs doby=new Dogs();
+            // doby.legs=4;
+            // System.out.println(doby.legs);
+            // System.out.println(doby);
+            Calculator calc =new Calculator();
+            
+            
         }
     }
-    class BankAccount{
-        public String username ;
-        private String password; 
-        public void setPassword(String pwd){
-            password = pwd; 
-           }
-    
-    
-    // }
-    class Pen{
-        private String color;
-        public int tip;
-    
-        Pen (String color){
-            this.color = color;
-        }
-    
-        // function for color change
-        void setcolor(String newColor){
-            color = newColor;    
-        }
-        String getcolor(){
-            return this.color;
-        }
-    }
-         // function for tip change
-        void setTip(int newTip){
-            tip = newTip;    
-        }
-        // getter and setters
-    
-        int getTip(){
-            return this.tip;
-        }
-    }
-    class Student{
-        String name;int age; float percentage;
-        void calPrcentage(int maths,int chem ,int phy){
-            percentage = (phy+ chem + maths)/3;
-        }
-    }
+
     
     //  constructor 
-    class Student{
-        String name;
-        int roll;
+    // class Student{
+    //     String name;
+    //     int roll;
+    //     String password;
+    //     int marks[];
+    //     Student(Student s1){
+    //         this.name=s1.name;
+    //         this.roll=s1.roll;
+    //         this.marks=s1.marks;
+
+    //     }
     
-        Student(String name){
-            this.name = name;
-        }
-        Student (String name){
-        }  
-        // return null
-    
-        // constructor overloading 
-        Student (){
-            System.out.print("constructor");
-        }
+        
+    //     Student (){
+    //         marks=new int[3];
+    //         System.out.println("constructor");
+    //     }
+    //     Student(String name){
+    //         marks=new int[3];
+    //         this.name = name;
+    //     }
+    // }
+// class Animal{
+//     void eat(){
+//         System.out.println("eats");
+
+//     }
+//     void breath(){
+//         System.out.println("breaths");       
+
+//     }
+// }
+// class Mammal extends Animal{
+//     int legs;
+// }
+// class Dogs extends Mammal{
+//     int bread;
+// }
+
+// class Fish extends Animal{
+
+//     void swim(){
+//         System.out.println("swims");
+//     }
+// }
+
+class Calculator{
+    int sum(int a,int b){
+        return a+b;
+    }
+    float sum(float a,float b){
+        return a+b;
+    }
+    int sum(int a,int b,int c){
+        return a+b+c;
     }
     
+}
