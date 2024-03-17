8fdab895457cc9554918fa433deb680ee383cd40
xoshivam <shivaXagarwal@gmail.com>
2024-03-17 11:47:32 +0000

    recusion intial

--- recursion.java
+++ recursion.java
@@ -1,18 +1,93 @@
-import java.util.*;
+// import java.util.*;
 
 public class recursion {
-    public static int printdec(int n) {
+// print factorial
+    // public static int printdec(int n) {
+    //     if (n==0) {
+    //         return 1;
+    //     }
+    //     return (printdec(n-1)*n);
+    // }
 
-        if (n==0) {
-            return 1;
+// print fibonacci series
+    // public static void fib(int n) {
+    //     for (int i = 0; i <n; i++) {
+    //         System.out.println("Answer = "+func(i));
+    //         }
+    // }
+    // public static int func(int n) {
+    //     if (n<=1) {
+    //         return n;
+    //     }
+    //     return func(n-1)+func(n-2);
+    // }
+
+// check the arr is sorted or not
+    // public static boolean sorted(int arr[],int i) {
+    //     if (i==arr.length-1) {
+    //         return true;
+    //     }
+    //     if (arr[i]> arr[i+1])
+    //     {
+    //         return false;
+    //     }
+    //     return sorted(arr, i+1);
+    // }
+
+// first occurence
+        // public static int firstoccurence (int arr[],int key,int i) {
+        //     if (i==arr.length) {
+        //         return -1;
+        //     }
+        //     if(arr[i]==key){
+        //         return i;
+        //     }
+        //     return firstoccurence(arr, key, i+1);
+
+        // }
+// last occurence
+    // public static int lastoccurence (int arr[],int key,int i) {
+    //     if (i==0) {
+    //         return -1;
+    //     }
+    //     if(arr[i]==key){
+    //         return i;
+    //     }
+    //     return lastoccurence(arr, key, i-1);
+
+    // }  
+    
+// print x^n
+        // public static int pow(int x , int n) {
+        //     if (n==0) {
+        //         return 1;
+        //     }
+        //     return x*pow(x, n-1);
+        // }
+// 
+        public static int pow(int x , int n) {
+            if (n==0) {
+                    return 1;
+                }
+            int halfpower=pow(x, n/2)*pow(x, n/2);
+            if (n%2!=0){
+                halfpower =x*halfpower;
+            }
+            return halfpower;
         }
-        return (printdec(n-1)*n);
-    }
+
     public static void main(String[] args) {
-        System.out.print("Enter the number which factorial is to be calculated :- ");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); 
-        sc.close();
-        System.out.println("Answer = "+printdec(n));
+        // System.out.print("Enter the number :- ");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt(); 
+        // sc.close();
+        // int n[]={1,3,4,2,2,6,1};
+        // int i=n.length-1;
+        // int f=firstoccurence(n,3,i);
+        // System.out.println(f);
+        System.out.println(pow(2,5));
+
+        
+        
     }
 }
