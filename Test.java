d4327ed733a2bcf5c8ec7f39079674d342b462f3
xoshivam <shivaXagarwal@gmail.com>
2024-03-19 14:33:12 +0000

    merge sort

--- Test.java
+++ Test.java
@@ -1,5 +1,18 @@
 public class Test{
- public static void main(String[] args) {
+ public static void insertionsort(int arr[]) {
+     for (int i = 1; i < arr.length; i++) {
+         int j=i-1;
+         int temp=arr[i];
+         while (j>=0 && arr[j]>temp) {
+             arr[j+1]=arr[j];
+             j--;
+         }
+         arr[j+1]=temp;
+     }
+
+   
+ }
+   public static void main(String[] args) {
     System.out.println("hello");
  }   
 }
---
+++ divideandconquer.java
@@ -0,0 +1,53 @@
+public class divideandconquer {
+    public static void merge(int arr[],int si,int mid,int ei) {
+        int temp[]=new int[ei-si+1];
+        int i=si;
+        int j=mid+1;
+        int k=0;
+        while (i<=mid && j<=ei) {
+            if (arr[i]<arr[j]) {
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<=mid) {
+            temp[k]=arr[i];
+            k++;
+            i++;
+        }
+        while (j<=ei) {
+            temp[k]=arr[j];
+            k++;
+            j++;
+        }
+        for (int l = 0; l < temp.length; l++) {
+            arr[si+l]=temp[l];
+        }
+    }
+    public static void mergesort(int arr[],int si,int ei) {
+        if (si<ei) {
+            int mid=si+(ei-si)/2;
+            mergesort(arr, si, mid);
+            mergesort(arr, mid+1, ei);
+            merge(arr, si, mid, ei);
+        }
+    }
+    public static void printarray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,11,7,8,1,0};
+        mergesort(arr, 0, arr.length-1);
+        // quicksort(arr, 0, arr.length-1);
+        printarray(arr);
+    }
+}
