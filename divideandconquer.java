bb77d4287130918456bf26a9adeae1b1ce9f0f76
xoshivam <shivaXagarwal@gmail.com>
2024-03-19 14:33:38 +0000

    quick sort

--- divideandconquer.java
+++ divideandconquer.java
@@ -43,6 +43,32 @@ public class divideandconquer {
         }
         System.out.println();
     }
+    public static int partition(int arr[],int si,int ei) {
+        int pivot=arr[ei];
+        int i=si-1;
+        for (int j = si; j < ei; j++) {
+            if (arr[j]<pivot) {
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[ei];
+        arr[ei]=temp;
+        return i+1;
+    }
+    // public static void 
+
+
+    public static void quicksort(int arr[],int si,int ei) {
+        if (si<ei) {
+            int pi=partition(arr, si, ei);
+            quicksort(arr, si, pi-1);
+            quicksort(arr, pi+1, ei);
+        }
+    }
 
     public static void main(String[] args) {
         int arr[] = {5,11,7,8,1,0};
