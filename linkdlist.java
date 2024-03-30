c6fd7e8170516a260d19eecf9ad79ae612c4edfc
xoshivam <shivaXagarwal@gmail.com>
2024-03-30 11:22:38 +0000

    remove search and reverse in linklist

--- linkdlist.java
+++ linkdlist.java
@@ -9,10 +9,12 @@ public class linkdlist {
     }
     public static node head;
     public static node tail;
+    public static int size;
     
     public void addfirst(int data){    //o(1)
          // step 1 create a new node
          node newnode=new node(data);
+         size++;
         if(head==null){
             head=tail=newnode;
             return;
@@ -25,6 +27,7 @@ public class linkdlist {
     }
     public void addlast(int data){    //o(1)
         node newnode=new node(data);
+        size++;
         if (head==null) {
             head=tail=newnode;
             return;
@@ -38,6 +41,7 @@ public class linkdlist {
             return;
         }
         node newnode=new node(data);
+        size++;
         node temp=head;
         if (head==null) {
             head=tail=newnode;
@@ -54,6 +58,8 @@ public class linkdlist {
         temp.next=newnode;
 
     }
+
+
     public void printlinklist(){
         if (head==null) {
             System.out.println("ll is empty");
@@ -65,22 +71,146 @@ public class linkdlist {
         }
         System.out.println("null");
     }
+
+
+    public void removefirst(){
+        int val;
+        if (head==null) {
+            return;
+        }
+        
+        else if(size==1){
+            val=head.data;
+            head=tail=null;
+            return; 
+        }
+       val=head.data;
+        System.out.println("removed data is "+val);
+        head=head.next;
+        size--;
+    }
+
+
+    public void removelast(){
+        int val;
+        if (head==null) {
+            return;
+        }
+        
+        else if(size==1){
+            val=tail.data;
+            head=tail=null;
+            return; 
+        }
+        val=tail.data;
+        node temp=head;
+        for (int count=0;count<size-2;count++) {
+            temp=temp.next;
+        }
+        temp.next=null;
+        tail=temp;
+        size--;
+        System.out.println("removed data is "+val);
+    }
+    public int searchitrative(int data){
+        if (head==null) {
+            System.out.println("ll is empty");
+            return -1;
+
+        }
+        node temp =head;
+        int idx=0;
+        while (temp!=null) {
+            
+            if (temp.data==data) {
+                return idx+1;
+            }
+            temp=temp.next;
+            idx++;
+
+            
+        }
+        return -1;
+    }
+    public int helper(node head,int key){
+        
+        if(head==null){
+            return -1;
+        }
+
+        if (head.data==key) {
+            return 0;
+        } 
+        int idx = helper(head.next, key);
+        if (idx ==-1) {
+            return -1;
+        }
+        return idx+1;
+    }
+    public int searchrecursive(int data ){
+        return helper(head,data);
+    }
+    public void reverse(){
+        node previous=null;
+        node current=tail=head;
+        while (current!=null) {
+        node next=current.next;
+        current.next=previous;
+        previous=current;
+        current=next;
+        }
+        head=previous;
+    }
+
+    public void removenthfromlast(int index){  //0(n)
+        int sz=0;
+        if (index==0) {
+            removefirst();
+            return;
+        }
+        node temp=head;
+        while (temp!=null) {
+            temp=temp.next;
+            sz++;
+        }
+        int count =1;
+        node temp1=head;
+        while (count<sz-index) {
+            
+            temp1=temp1.next;
+            count++;
+        }
+        temp1.next=temp1.next.next;
+        return;
+
+
+    }
     public static void main(String[] args) {
         linkdlist l1=new linkdlist();
         
         l1.printlinklist();
-        l1.addfirst(1);
-        l1.printlinklist();
         l1.addfirst(2);
         l1.printlinklist();
-        l1.addfirst(5);
+        l1.addfirst(1);
+        l1.printlinklist();
+        l1.addlast(4);
         l1.printlinklist();
         l1.addlast(6);
         l1.printlinklist();
         l1.addlast(7);
         l1.printlinklist();
-        l1.addmiddle(3,5);
+        l1.addmiddle(2,3);
         l1.printlinklist();
+        // l1.removefirst();
+        // l1.printlinklist();
+        // l1.removelast();
+        // l1.printlinklist();
+        // System.out.println(l1.searchrecursive(4));
+        // l1.reverse();
+        l1.removenthfromlast(3);
+        l1.printlinklist();
+
+        System.out.println(size);
 
     }    
 }
