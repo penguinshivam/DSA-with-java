f25d0c03bfa01accc30654bdd971f4e51de4d2d3
Shivam Agarwal <shivaXagarwal@gmail.com>
2024-04-12 13:32:55 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,57 @@
+import java.util.*; 
+ public class javaBasic2 {
+    public static void main (String agrs[]){
+
+    
+    // OPERATORS
+
+    // BINARY
+    int A = 2;
+    int B = 5;
+    System.out.println( "add = " + (A *( B / A)));
+    System.out.println( "sub = " + (A - B));
+    int a = 10;
+    int b = ++a;
+    System.out.println(a);
+    System.out.println(b);
+
+    // UNARY 
+    // int a = 10;
+    // int b = a++;
+    // int c = ++a;  
+    // int d = --a;
+    // int e = a--;
+    // System.out.println(a);
+    // System.out.println(b);
+    // System.out.println(c);
+    // System.out.println(d);
+    // System.out.println(e);
+
+    // RELATIONAL
+    // int a = 10; 
+    // int b =5;
+    // System.out.println( a == b);
+    // System.out.println( a != b);
+    // System.out.println( a > b);
+    // System.out.println( a < b);
+    // System.out.println( a >= b);
+    // System.out.println( a <= b);
+
+    // LOGICAL 
+    // System.out.println(( 3>2) && (6>2));
+    // System.out.println(( 3>2) || (6<2)); 
+    // System.out.println(!( 3>2));
+
+    // aASSIGNMENT
+    // int a = 10;
+    // int b = 10;
+    // // a =  a + 10;
+    //  a += 10;
+    //  b -= 5;
+    // System.out.println(a);
+    // System.out.println(b);
+
+
+
+}
+}
