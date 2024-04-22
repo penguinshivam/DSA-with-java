cd28fc292936e80e375dc4f13fcb18134ecd4ab0
penguinshivam <shivaxagarwal@gmail.com>
2024-04-22 23:57:33 +0530

    circular queue

--- Queue.java
+++ Queue.java
@@ -3,20 +3,32 @@ public class Queue {
         static int arr[];
         static int size;
         static int rear;
-        
+        static int front;
+
         Que(int n){
             arr = new int [n];
             size=n;
             rear=-1;
+            front=-1;
         }
         public boolean isEmpty(){
-            return rear==-1;
+            return rear==-1&&front ==-1;
+        }
+        public boolean isFull(){
+            return (rear+1)%size==front;
         }
         public void add(int data){
-            if (rear == size-1){
-                System.out.println("queue is full");
+            if (isFull()) {
+                System.out.println("isFull");
+                return;
             }
-            rear=rear+1;
+            // if (rear == size-1){
+            //     System.out.println("queue is full");
+            // }
+            if (front==-1) {
+                front =0;   
+            }
+            rear=(rear+1)%size;
             arr[rear]=data;
 
         }
@@ -25,13 +37,17 @@ public class Queue {
                 System.out.println("empty queue");
                 return -1;
             }
-            int front =arr[0];
-            for (int i = 0; i < rear; i++) {
-                arr[i]=arr[i+1];
-
+            int result =arr[front];
+            // for (int i = 0; i < rear; i++) {
+            //     arr[i]=arr[i+1];
+            // }
+            if (rear==front) {
+                rear = front =-1;
+            }else
+            {
+                front=(front+1)%size;
             }
-            rear=rear-1;
-            return front;
+            return result;
 
         }
         public int peek(){
@@ -39,19 +55,22 @@ public class Queue {
                 System.out.println("empty queue");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
 
     }
     public static void main(String[] args) {
-        Que q= new Que(5);
+        Que q= new Que(3);
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
         while (!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
         }
-        q.remove();
     }
 }
