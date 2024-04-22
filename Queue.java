0970d6494f9e5158eafb5ba422844f7f0312a2ba
penguinshivam <shivaxagarwal@gmail.com>
2024-04-22 22:59:49 +0530

    implementation of queue using array

---
+++ Queue.java
@@ -0,0 +1,57 @@
+public class Queue {
+    static class Que{
+        static int arr[];
+        static int size;
+        static int rear;
+        
+        Que(int n){
+            arr = new int [n];
+            size=n;
+            rear=-1;
+        }
+        public boolean isEmpty(){
+            return rear==-1;
+        }
+        public void add(int data){
+            if (rear == size-1){
+                System.out.println("queue is full");
+            }
+            rear=rear+1;
+            arr[rear]=data;
+
+        }
+        public int remove(){
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            int front =arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i]=arr[i+1];
+
+            }
+            rear=rear-1;
+            return front;
+
+        }
+        public int peek(){
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return arr[0];
+        }
+
+    }
+    public static void main(String[] args) {
+        Que q= new Que(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+        q.remove();
+    }
+}
