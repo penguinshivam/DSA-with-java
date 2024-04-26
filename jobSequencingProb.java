6babd6e1dbff214f00516f9871fc69a364ca91a1
penguinshivam <shivaxagarwal@gmail.com>
2024-04-26 23:04:56 +0530

    job Sequencing Problem using greedy approch

---
+++ jobSequencingProb.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Job{
+   int deadline;
+   int profit;
+   int id;
+
+   public Job(int i , int d, int p){
+      id = i;
+      deadline = d;
+      profit = p;
+   }
+}
+public class jobSequencingProb {
+      
+    public static void main(String[] args) {
+        int jobInfo[][] = {{4,20},{1,10},{1,40},{1,30}};
+
+      //   Job jobs[] = new Job[jobInfo.length];
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        for( int i = 0 ; i< jobInfo.length;i++){
+            jobs.add(new Job(i, jobInfo[i][0],jobInfo[i][1]));
+        }
+
+        Collections.sort(jobs,(o1,o2) ->o2.profit - o1.profit); //descending order of profit
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for( int i = 0 ; i< jobs.size();i++){
+            Job curr = jobs.get(i);
+            if( curr.deadline> time){
+                seq.add(curr.id);
+                time++;
+            }
+        }
+
+        // print seq
+        System.out.println("max jobs = "+ seq.size());
+        for( int i = 0 ; i< seq.size();i++){
+            System.out.print(seq.get(i)+ " ");
+        }
+        System.out.println();
+        }
+
+}
