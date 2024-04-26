d87dcb9e0b85a24b75ac565b5ed83fc87a5700a3
penguinshivam <shivaxagarwal@gmail.com>
2024-04-26 23:28:21 +0530

    Max Len Chain of pairs in Greedy Approch

--- maxLenChainOfPairs.java
+++ maxLenChainOfPairs.java
@@ -1,25 +1,17 @@
 import java.util.*;
 public class maxLenChainOfPairs{
     public static void main(String[] args) {
-        int start[]={5,39,5,27,50};
-        int end[]={24,60,28,40,90};
-
         // sorting
-        int activity[][]= new int [start.length][2];
-
-        for (int i = 0; i < start.length; i++) {
-            activity[i][0]=start[i];
-            activity[i][1]=end[i];
-        }
-        Arrays.sort(activity, Comparator.comparingDouble(o -> o[1]));
+        int pairs[][]= {{5,24},{39,60},{5,28},{27,40},{50,90}};
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
 
 
         int maxlen=1;
-        int lastEnd= activity[0][1];
-        for (int i = 0; i < end.length; i++) {
-            if (activity[i][0]>lastEnd) {
+        int lastEnd= pairs[0][1];
+        for (int i = 0; i < pairs.length; i++) {
+            if (pairs[i][0]>lastEnd) {
                 maxlen++;
-                lastEnd=activity[i][1];
+                lastEnd=pairs[i][1];
             }
         } 
         System.out.println(maxlen);
