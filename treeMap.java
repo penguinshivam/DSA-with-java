910554ee77b7240cfc195ad942636ea6947eda56
penguinshivam <shivaxagarwal@gmail.com>
2024-05-15 22:42:50 +0530

    TreeMap on java

---
+++ treeMap.java
@@ -0,0 +1,14 @@
+// import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class treeMap {
+    public static void main(String[] args) {
+        TreeMap<String,Integer> tm = new TreeMap<>();
+        tm.put("India",100);
+        tm.put("USA",150);
+        tm.put("UK",50);
+        tm.put("China",10); 
+        System.out.println(tm);
+        
+    }
+}
